e03246f8457d26ef8c61b23e1f3bd324b64a3911
JinHyuk <mjh7131@naver.com>
2023-01-26 00:30:37 +0900

    SICP 연습문제 1.3 수정

--- 1_3.scm
+++ 1_3.scm
@@ -1,11 +1,8 @@
-(define (avg a b c)
-  (/ (+ a b c) 3))
-
 (define (square a) (* a a))
 
 (define (f a b c)
-  (cond (< a (avg a b c)) (+ (square b) (square c))
-        (< b (avg a b c)) (+ (square a) (square c))
-        (< c (avg a b c)) (+ (square b) (square a))))
+  (cond ((and (< a b) (< a c)) (+ (square b) (square c)))
+        ((and (< b a) (< b c)) (+ (square a) (square c)))
+        ((and (< c a) (< c b)) (+ (square b) (square a)))))
 
-(f 1 2 3) 
+(f 1 2 3)
